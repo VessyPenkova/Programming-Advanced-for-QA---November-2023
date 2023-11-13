246d957d24f7e8be82b74d3dfa47a88fd2e66f03
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-11-13 21:42:38 +0200

    04._Text_Filter | Strings and Text Processing - Lab | Replace

---
+++ 04. Text Filter/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+
+            string stringToSubstring = Console.ReadLine();
+
+            foreach (var bannedWord in bannedWords)
+            {
+                if (stringToSubstring.Contains(bannedWord))
+                {
+                    stringToSubstring = stringToSubstring.Replace(bannedWord,
+                        new string('*', bannedWord.Length));
+                }
+            }
+            Console.WriteLine(stringToSubstring);
+        }
+    }
+}
