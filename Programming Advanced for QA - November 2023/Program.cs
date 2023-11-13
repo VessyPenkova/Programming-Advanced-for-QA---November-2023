2a86a3efe37f5a223b4917b760d255918c36157b
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-11-13 20:46:26 +0200

    Add project files.

---
+++ Programming Advanced for QA - November 2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programming_Advanced_for_QA___November_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
