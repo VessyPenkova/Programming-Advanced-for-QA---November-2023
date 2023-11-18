4cece6506035201cf7aa48e68126de76a2043c69
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-11-18 11:16:04 +0200

    _01._Match_Full_Name | Regular Expressions - Lab Added

---
+++ 01. Match Full Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            MatchCollection match = regex.Matches(text);
+
+            Console.WriteLine(string.Join(" ", match));
+        }
+    }
+}
