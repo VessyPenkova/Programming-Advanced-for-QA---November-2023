944b8a4ede0684792dfdcb7dc8b2569ccbb79922
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-11-19 19:52:45 +0200

    02. A Miner Task | Dictionaries, Lambda and LINQ - Exercise Added

---
+++ 02. A Miner Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "";
+            Dictionary<string, int> minnerTask = new();
+
+            while (key != "stop")
+            {
+                key = Console.ReadLine();
+                if (key == "stop")
+                {
+                    break;
+                }
+                int value = int.Parse(Console.ReadLine());
+
+                if (minnerTask.ContainsKey(key))
+                {
+                    minnerTask[key] += value;
+                }
+                else
+                {
+                    minnerTask[key] = value;
+                }
+            }
+
+            foreach (var item in minnerTask)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
