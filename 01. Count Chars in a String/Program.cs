68175cefea5f16cc4b276e71e1055805f99a8297
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-11-19 19:14:32 +0200

    01. Count Chars in a String | Dictionaries, Lambda and LINQ - Exercise Added

---
+++ 01. Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordSequences = Console.ReadLine();
+          
+             Dictionary<char, int> counts = new();
+
+            foreach (char ch in wordSequences)
+            {
+                if (counts.ContainsKey(ch))
+                {
+                    counts[ch] += 1;
+                }
+                else
+                {
+                    counts[ch] = 1;
+                }
+            }
+            foreach (var count in counts)
+            {
+                if (count.Key != ' ')
+                {
+                    Console.WriteLine($"{count.Key} -> {count.Value}");
+                }
+                
+            }
+        }
+    }
+}
