c96b0a751d88bc221232e13a49f9de9af7f11911
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-11-23 20:26:31 +0200

    Dictionaries, Lambda and LINQ - Unit Testing Exercise 01. Songs | Objects and Classes - Lab Added

---
+++ 01. Car/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+            Console.WriteLine($"Make: {firstCar.Make}\nModel: {firstCar.Model}\n: {firstCar.Year}");
+            Console.WriteLine(firstCar.WhoAmI());
+        }       
+    }
+}
