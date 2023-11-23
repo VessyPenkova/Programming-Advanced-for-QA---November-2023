14b669776a9247c2589522cecd6ae56f4a8c3652
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-11-23 22:04:48 +0200

    03. Store Boxes | Objects and Classes - Lab Added

---
+++ 03. Store Boxes/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _03._Store_Boxes
+{
+    public class Box
+    {
+        public Box(string serilaNumber, Item item, int itemQuantity)
+        {
+            SerialNumber = serilaNumber;
+            Item = item;
+            ItemQuantity = itemQuantity;
+            PriceforBox = item.Price * itemQuantity;
+        }
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity  { get; set; }
+
+        public decimal PriceforBox { get; set; }
+    }
+}
+
+// Serial Number, Item, Item Quantity and Price for a Box.
---
+++ 03. Store Boxes/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _03._Store_Boxes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            List<Box> boxes = new List<Box>();
+
+            while (command != "end")
+            {
+                string[] inputParts = command.Split(" ");
+                string serialNumber = inputParts[0];
+                string itemName = inputParts[1];
+                int itemQuantity = int.Parse(inputParts[2]);
+                decimal itemPrice = decimal.Parse(inputParts[3]);
+
+                Item currentItem = new Item(itemName, itemPrice);
+                Box currentBox = new Box(serialNumber, currentItem, itemQuantity);
+
+                boxes.Add(currentBox);
+
+                command = Console.ReadLine();
+            }
+
+            boxes = boxes.OrderByDescending(box => box.PriceforBox).ToList();
+
+            foreach (Box box in boxes)
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceforBox:f2}");
+            }
+        }
+        public class Item
+        {
+            public Item(string name, decimal price)
+            {
+                Name = name;
+                Price = price;
+            }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+        public class Box
+        {
+            public Box(string serilaNumber, Item item, int itemQuantity)
+            {
+                SerialNumber = serilaNumber;
+                Item = item;
+                ItemQuantity = itemQuantity;
+                PriceforBox = item.Price * itemQuantity;
+            }
+            public string SerialNumber { get; set; }
+            public Item Item { get; set; }
+            public int ItemQuantity { get; set; }
+
+            public decimal PriceforBox { get; set; }
+        }
+    }
+}
