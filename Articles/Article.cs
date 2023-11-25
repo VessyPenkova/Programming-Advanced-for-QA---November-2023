1e02ca030f11a429a27585c1717a016aca82b48c
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-11-25 18:24:13 +0200

    02. Articles | Objects and Classes - Exercise | Added

---
+++ Articles/Article.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Articles
+{
+    public class Article
+    {
+        //public Article(string title, string content, string author)
+        //{
+        //    this.Author = author;
+        //    this.Content = content;
+        //    this.Author = author;
+        //}
+        string title;
+        string content;
+        string author;
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public string Edit(string newContent)
+        {
+            this.Content = newContent;
+            return this.Content;
+        }
+        public string ChangeAuthor(string newAuthor)
+        {
+            this.Author = newAuthor;
+            return this.Author;
+        }
+        public string Rename(string newTitle)
+        {
+            this.Title = newTitle;
+            return this.Title;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{0} - {1}: {2}", this.Title, this.Content, this.Author);
+        }
+    }
+}
---
+++ Articles/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Articles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputInfo = Console.ReadLine();
+
+            string[] infoParts = inputInfo.Split(", ").ToArray();
+
+            string currenttTitle = infoParts[0];
+            string currentContent = infoParts[1];
+            string currentAuthor = infoParts[2];
+
+            Article currentarticle = new Article()
+            {
+                Title = currenttTitle,
+                Content = currentContent,
+                Author = currentAuthor
+            };
+
+            int numberOFinputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOFinputs; i++)
+            {
+                string[] commands = Console.ReadLine().Split(": ").ToArray();
+                string command = commands[0];
+                if (command == "Edit")
+                {
+                    string newContent = commands[1];
+                    currentarticle.Edit(newContent);
+                    //currentarticle.Title = currenttTitle;
+                    //currentarticle.Author = currentAuthor;
+                }
+                if (command == "ChangeAuthor")
+                {
+                    string newAuthor = commands[1];
+                    currentarticle.ChangeAuthor(newAuthor);
+                    //currentarticle.Content = currentContent;
+                    //currentarticle.Title = currenttTitle;
+                }
+                if (command == "Rename")
+                {
+                    string newTitle = commands[1];
+                    currentarticle.Rename(newTitle);
+                    //currentarticle.Author = currentAuthor;
+                    //currentarticle.Content = currentContent;
+                }
+            }
+            Console.WriteLine($"{currentarticle.Title} - {currentarticle.Content}: {currentarticle.Author}");
+        }
+    }
+}
