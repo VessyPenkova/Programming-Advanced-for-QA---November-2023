7dd56f5b6e4a4338cb9a448d866c44147b74f6dd
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-12-01 16:53:19 +0200

    03. Teamwork Projects | Objects and Classes - Exercise

--- Teamwork Projects/Program.cs
+++ Teamwork Projects/Program.cs
@@ -8,7 +8,7 @@ namespace Teamwork_Projects
         {
             int countTeams = int.Parse(Console.ReadLine());
 
-            Dictionary< string, Team> teams = new Dictionary< string, Team>();
+            Dictionary<string, Team> teams = new Dictionary<string, Team>();
 
             for (int i = 0; i < countTeams; i++)
             {
@@ -16,65 +16,78 @@ namespace Teamwork_Projects
                 string creator = info[0];
                 string teamName = info[1];
 
-                if (!teams.Keys.Contains(teamName))
-                {
-                    Team team = new Team(teamName, creator);
-                    Console.WriteLine($"Team {teamName} has been created by {creator}!");
-                    teams.Add(teamName, team);
-                }
                 if (teams.Keys.Contains(teamName))
                 {
                     Console.WriteLine($"Team {teamName} was already created!");
-                }    
-                //if (teams[teamName].Members.Contains(creator))
-                //{
-                //    if (teams[teamName].Members.Contains(creator))
-                //    {
-                //        Console.WriteLine($"{creator} cannot create another team!");
-                //    }
-                //}                
+
+                }
+                else if (teams.Any(t => t.Value.Creator == creator))
+                {
+                    Console.WriteLine($"{creator} cannot create another team!");
+                }
+                else
+                {
+                    Team team = new Team(teamName, creator);
+                    teams.Add(teamName, team);
+                    Console.WriteLine($"Team {teamName} has been created by {creator}!");
+                }
+
             }
-            string command = Console.ReadLine();
-            while (command != "end of assignment")
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "end of assignment")
             {
                 string[] parts = command.Split("->");
                 string currentUserToJoin = parts[0];
                 string nameOfTheTeamToJoinIn = parts[1];
+
                 if (!teams.Keys.Contains(nameOfTheTeamToJoinIn))//Check if the Team Existing
                 {
                     Console.WriteLine($"Team {nameOfTheTeamToJoinIn} does not exist!");
+                    continue;
                 }
-                if () )//Check if the currentUser is in another Team
+
+                var userIsCreator = teams[nameOfTheTeamToJoinIn].Creator == currentUserToJoin;
+                var isUserAlreadyExist = teams.Values.Any(t => t.Members.Contains(currentUserToJoin));
+
+                if (userIsCreator || isUserAlreadyExist)
                 {
                     Console.WriteLine($"Member {currentUserToJoin} cannot join team {nameOfTheTeamToJoinIn}!");
+                    continue;
                 }
-                else if (teams.Keys.Contains(nameOfTheTeamToJoinIn))
+
+                var validTeams = teams
+                    .Where(t => t.Value.Members.Count > 0)
+                    .OrderBy(t => t.Value.Name)
+                    .ToDictionary(t => t.Key, t => t.Value);
+
+                var emptyTeams = teams
+                 .Where(t => t.Value.Members.Count == 0)
+                .OrderBy(t => t.Value.Name)
+                .ToDictionary(t => t.Key, t => t.Value);
+
+
+                //3. Printing
+                //1 type Teams without Members
+                //2 type Teams with Members 
+
+                foreach (var team in validTeams.Values.OrderByDescending(t => t.Members.Count))
                 {
-                    teams[nameOfTheTeamToJoinIn].Members.Add(currentUserToJoin);
+                    Console.WriteLine($"{team.Name}");
+                    Console.WriteLine($"- {team.Creator}");
+
+                    foreach (var member in team.Members.OrderBy(m => m))
+                    {
+                        Console.WriteLine($"-- {member}");
+                    }
                 }
 
+                Console.WriteLine("Teams to disband:");
 
-                command = Console.ReadLine();
-            }
-
-            //3. Printing
-            //1 type Teams without Members
-            //2 type Teams with Members 
-
-            foreach (var team in teams.Where(team => team.Value.Members.Count > 0)
-                .OrderByDescending(team => team.Value.Members.Count)
-                .ThenBy(team => team.Key))
-            {
-                Console.WriteLine(team.Key);
-                Console.WriteLine("- " + team.Value.Creator);
-
-            }
-
-            Console.WriteLine("Teams to disband: ");
-            foreach (var team in teams.Where(team => team.Value.Members.Count == 0))
-            {
-                Console.WriteLine(team.Key);
-            }
+                foreach (var emptyTeam in emptyTeams.Values)
+                {
+                    Console.WriteLine($"{emptyTeam.Name}");
+                }
+            }          
         }
     }
 }
--- Teamwork Projects/Team.cs
+++ Teamwork Projects/Team.cs
@@ -8,14 +8,15 @@ namespace Teamwork_Projects
 {
     public class Team
     {
-        public string Name { get; set; }
-        public string Creator { get; set; }
-        public List<Member> Members { get; set; }
         public Team(string name, string creator)
         {
             Name = name;
             Creator = creator;
-            Members = new List<Member>();
+            Members = new List<string>();
         }
+        public string Name { get; set; }
+        public string Creator { get; set; }
+        public List<string> Members { get; set; }
+       
     }
 }
