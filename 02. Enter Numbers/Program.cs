2db67bd2bca145f61a089359d486c11498bfb94c
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-12-02 13:13:46 +0200

    Exceptions and Error Handling - Lab | 02. Enter Numbers | Added

--- 02. Enter Numbers/Program.cs
+++ 02. Enter Numbers/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace _02._Enter_Numbers
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _02._Enter_Numbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            static int ReadNumber(int startRange, int endRange)            
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number <= startRange || number >= endRange)
+                {
+                    throw new ArgumentException();
+                }
+
+                return number;
+            }
+
+            List<int> numbers = new List<int>();
+
+            int previousNumber = 1;
+
+            while (numbers.Count < 10)
+            {
+                try
+                {
+                    int number = ReadNumber(previousNumber, 100);
+
+                    numbers.Add(number);
+                    previousNumber = number;
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine($"Your number is not in range {previousNumber} - 100!");
+                }
+                catch (FormatException)
+                {
+
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+            Console.WriteLine(String.Join(", ", numbers));   
         }
     }
 }
+
+//=============================================================
+
+//string numberToCheck = Console.ReadLine();
+//int numberToCheckInt;
+//bool IsIntteger = int.TryParse(numberToCheck, out numberToCheckInt);
+
+//==========================================================================
+
